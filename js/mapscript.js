74cb6259e1d6c8b584bd5bc6c1d449160207cd67
Cisse16 <98100632+Cisse16@users.noreply.github.com>
2024-12-14 21:57:20 +0100

    Add files via upload

---
+++ js/mapscript.js
@@ -0,0 +1,20 @@
+let mapje = L.map('mapje').setView([51.08870544582537, 4.372741442029028], 15);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapje);
+
+
+/*veranderd kleur van marker heb dit opgezocht op google  */
+var greenIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
+
+
+  L.marker([51.08870544582537, 4.372741442029028], {icon: greenIcon}).addTo(mapje);
